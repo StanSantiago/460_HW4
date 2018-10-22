567cb77082e5497b9bff562c36625fdc8e2e4d33
AnEntireSkeleton <AnEntireSkeleton@users.noreply.github.com>
2018-10-21 18:32:57 -0700

    Added conversion functionality

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,29 @@ namespace WebApplication1.Controllers
 
         public ActionResult Converter()
         {
-            ViewBag.Message = "";
+
+            double final = 0;
+
+            string metric = Request.QueryString["Metric"];
+            int miles = Convert.ToInt32(Request.QueryString["Miles"]);
+
+            final = miles * 1.60934;
+
+            if (metric == "millimeters")
+                final *= 1000000;
+            else if (metric == "centimeters")
+                final *= 100000;
+            else if (metric == "meters")
+                final *= 1000;
+            else if (metric == "kilometers")
+            { }
+            else
+            {
+                ViewBag.Message = "";
+                return View();
+            }
+
+            ViewBag.Message = miles + " miles is " + final + " " + metric;
 
             return View();
         }
